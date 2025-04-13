9e13cafa007f01128974254973e5754ad14a3d99
Oliver Payne <olipayne@gmail.com>
2025-04-13 22:41:40 +0200

    Build: Configure manual chunks for optimization

--- cheerchaser-app/vite.config.ts
+++ cheerchaser-app/vite.config.ts
@@ -1,17 +1,37 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('leaflet') || id.includes('leaflet-routing-machine')) {
+              return 'leaflet-map';
+            }
+            if (id.includes('@turf')) {
+              return 'turf';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('gpxparser')) {
+              return 'gpxparser';
+            }
+          }
+        },
+      },
     },
   },
 })
